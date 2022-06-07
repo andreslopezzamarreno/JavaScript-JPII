baecd0a28f4f7f13cbce816ca7c4d5938040a50c
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2022-06-07 09:07:51 +0200

    Update ejercicio.js

--- 01_inicio/utils/js/ejercicio.js
+++ 01_inicio/utils/js/ejercicio.js
@@ -18,15 +18,15 @@ let numero2 = prompt("Introduce el valor 2");
 let confirmacion = confirm('Quieres realizar las operaciones');
 
 if (confirmacion){
-    let suma = (numero1 + numero2);
+    let suma = numero1 + numero2;
     let resta = numero1 - numero2;
     let multiplicacion =numero1 * numero2;
     let division = numero1 / numero2;
     let modulo = numero1 % numero2;
 
     console.log(`la suma es ${suma}`);
-    console.log(resta);
-    console.log(division);
-    console.log(multiplicacion);
-    console.log(modulo);
+    console.log(`la resltiplicacion es ${suma}`);
+    console.log(`la mues ${suma}`);
+    console.log(`la suma es ${suma}`);
+    console.log(`la suma es ${suma}`);
 }
