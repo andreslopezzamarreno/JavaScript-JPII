492b5c2a88ece9add687f24850ed6f3e217e7cc6
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2022-06-13 10:47:54 +0200

    update

--- 02_dinamismo/utils/js/eventos.js
+++ 02_dinamismo/utils/js/eventos.js
@@ -1,24 +1,40 @@
-let boton = document.querySelector('#boton_agregar');
+let boton = document.querySelector("#boton_agregar");
 
-/* boton.addEventListener("click",function () {
-    console.log('boton pulsado');
-}); */
+let input_agregar = document.querySelector("#texto_li");
 
-boton.addEventListener("click",(item) => {console.log('boton pulsado')});
+boton.addEventListener("click", function (element) {
+  
+    let contenido = input_agregar.value;
+    
+    //console.log(contenido);
+    //agregar nodos
+    /*  let lista = document.querySelector("#listaPrimero");
+    let nodoLi = document.createElement('li');
+    nodoLi.innerText = contenido;
+    nodoLi.classList.add("list-group-item");
+    lista.appendChild(nodoLi); */
 
-
-let input_agregar = document.querySelector('#texto_li');
-
-
-input_agregar.addEventListener("keyup",(item) => {
-    console.log(input_agregar.value);
+    //modificar el html
+    if (contenido != '') {
+    let lista = document.querySelector("#listaPrimero");
+    lista.innerHTML = `${lista.innerHTML} <li class = "list-group-item">${contenido}</li>`;
+    }else{
+        alert('tienes que meter un valor')
+    }
 });
+
+//boton.addEventListener("click",(item) => {console.log('boton pulsado')});
+
+/* input_agregar.addEventListener("keyup",(item) => {
+    
+    console.log(input_agregar.value);
+}); */
 
 /* en el parrafo que esta configurado al final en el 0 
 debera aparecer el numero de pulsaciones */
 
-let contador = document.querySelector('span');
+/* let contador = document.querySelector('span');
 
 input_agregar.addEventListener("keyup",(item) => {
     contador.innerText ++;
-}); 
+});  */
